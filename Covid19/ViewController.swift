0e1c0d205a56b645d0fc9c4633e37c17ccdb03f6
ehwah <soewah1996@gmail.com>
2020-04-05 18:10:37 -0400

    Initial Commit

---
+++ Covid19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid19
+//
+//  Created by sowah on 4/5/20.
+//  Copyright © 2020 sowah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
